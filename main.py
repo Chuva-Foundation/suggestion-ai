8a59a68a50a84e2ca9299c8dccc94453ddebeaa3
Stefan Vitoria <seacod3@sapo.pt>
2023-08-04 12:17:07 -0100

    feat: enabled conversation with AI. description:  created static file to feed ai with information to be used as knowledge to answer user prompts about product suggestions

--- main.py
+++ main.py
@@ -1,1 +1,55 @@
-from fastapi import FastAPI
+import os
+import sys
+
+import openai
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from dotenv import load_dotenv, find_dotenv
+from langchain.vectorstores import Chroma
+
+# import constants
+
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+# Enable to save to disk & reuse the model (for repeated queries on the same data)
+PERSIST = False
+
+query = None
+if len(sys.argv) > 1:
+    query = sys.argv[1]
+
+if PERSIST and os.path.exists("persist"):
+    print("Reusing index...\n")
+    vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+    index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+else:
+    # loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
+    # loader = DirectoryLoader("data/")
+    loader = PyPDFLoader("./fake-db/cachupa.pdf")
+    if PERSIST:
+        index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory": "persist"}).from_loaders([loader])
+    else:
+        index = VectorstoreIndexCreator().from_loaders([loader])
+
+chain = ConversationalRetrievalChain.from_llm(
+    llm=ChatOpenAI(model="gpt-3.5-turbo"),
+    retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+)
+
+chat_history = []
+while True:
+    if not query:
+        query = input("Prompt: ")
+    if query in ['quit', 'q', 'exit']:
+        sys.exit()
+    result = chain({"question": query, "chat_history": chat_history})
+    print(result['answer'])
+
+    chat_history.append((query, result['answer']))
+    query = None
